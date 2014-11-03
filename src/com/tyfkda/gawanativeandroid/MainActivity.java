3017126b92f97d301a0c4d8063bba4676fd7f848
tyfkda <tyfkda@gmail.com>
2014-11-03 17:48:24 +0900

    WebView配置、URL文字列によるページ読み込み

--- src/com/tyfkda/gawanativeandroid/MainActivity.java
+++ src/com/tyfkda/gawanativeandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.tyfkda.gawanativeandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    WebView webView = (WebView) findViewById(R.id.webView);
+    webView.loadUrl("http://www.example.com/");
   }
 }
