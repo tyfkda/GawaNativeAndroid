611fc8c042a96c868601d393a45d74ef0b6f494a
tyfkda <tyfkda@gmail.com>
2014-11-03 19:36:41 +0900

    WebViewClient#shouldOverrideUrlLoading でURLの遷移を捕捉

--- src/com/tyfkda/gawanativeandroid/MainActivity.java
+++ src/com/tyfkda/gawanativeandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.tyfkda.gawanativeandroid;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
   @Override
@@ -13,6 +16,15 @@ public class MainActivity extends Activity {
     WebView webView = (WebView) findViewById(R.id.webView);
     webView.getSettings().setJavaScriptEnabled(true);
     webView.addJavascriptInterface(new JavaScriptInterface(this, webView), "Native");
+    webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (!Uri.parse(url).getScheme().equals("native"))
+          return false;
+        Toast.makeText(MainActivity.this, "Url requested: " + url, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
     webView.loadUrl("file:///android_asset/index.html");
   }
 }
