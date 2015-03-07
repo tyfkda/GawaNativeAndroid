3fbf3caefaeaf1ee4a996270875512a5f072ef6f
tyfkda <tyfkda@gmail.com>
2015-03-07 22:04:29 +0900

    JavaからのJSのコードの呼び出しは、layoutで配置したボタンを押されたらにする

--- src/com/tyfkda/gawanativeandroid/MainActivity.java
+++ src/com/tyfkda/gawanativeandroid/MainActivity.java
@@ -2,9 +2,13 @@ package com.tyfkda.gawanativeandroid;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -13,9 +17,9 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     
-    WebView webView = (WebView) findViewById(R.id.webView);
+    final WebView webView = (WebView) findViewById(R.id.webView);
     webView.getSettings().setJavaScriptEnabled(true);
-    webView.addJavascriptInterface(new MyJavaScriptInterface(this, webView), "Native");
+    webView.addJavascriptInterface(new MyJavaScriptInterface(this), "Native");
     webView.setWebViewClient(new WebViewClient() {
       @Override
       public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -26,5 +30,20 @@ public class MainActivity extends Activity {
       }
     });
     webView.loadUrl("file:///android_asset/index.html");
+    
+    Button button = (Button) findViewById(R.id.button);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        evaluateJs(webView, "addTextNode('[Button clicked]')");
+      }
+    });
+  }
+
+  private void evaluateJs(WebView webView, String script) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+      webView.evaluateJavascript(script, null);
+    else
+      webView.loadUrl("javascript:" + script);
   }
 }
--- src/com/tyfkda/gawanativeandroid/MyJavaScriptInterface.java
+++ src/com/tyfkda/gawanativeandroid/MyJavaScriptInterface.java
@@ -1,31 +1,18 @@
 package com.tyfkda.gawanativeandroid;
 
 import android.content.Context;
-import android.os.Build;
 import android.webkit.JavascriptInterface;
-import android.webkit.WebView;
 import android.widget.Toast;
 
 public class MyJavaScriptInterface {
   private Context context;
-  private WebView webView;
   
-  public MyJavaScriptInterface(Context context, WebView webView) {
+  public MyJavaScriptInterface(Context context) {
     this.context = context;
-    this.webView = webView;
   }
 
   @JavascriptInterface
   public void showToast(String string) {
     Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
-
-    evaluateJs("addTextNode('" + string + "')");
-  }
-
-  private void evaluateJs(String script) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-      webView.evaluateJavascript(script, null);
-    else
-      webView.loadUrl("javascript:" + script);
   }
 }
